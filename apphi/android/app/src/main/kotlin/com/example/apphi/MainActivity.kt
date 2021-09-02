fed3a277c7df29530f23b15b6a3e509d5d941e87
tsy7 <82978748+tsy7@users.noreply.github.com>
2021-09-02 11:31:12 -0700

    Add files via upload

---
+++ apphi/android/app/src/main/kotlin/com/example/apphi/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.apphi
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ apphi/lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+        appBar: AppBar(
+          title: Text("I am Rich"),
+          backgroundColor: Colors.black,
+        ),
+        backgroundColor: Colors.white,
+        body: Center(
+          child: Text('Hello world'),
+        )),
+  ));
+}
